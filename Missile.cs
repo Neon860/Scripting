42a43291d85796c1c641313b6d5e0ab8494c6e6b
Neon860 <neonvaprolit@gmail.com>
2025-01-22 17:45:11 +0500

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+        Destroy(gameObject);
+    }
+}
