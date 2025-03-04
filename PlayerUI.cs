a644a413c400bcff5e8b5d18be645a4b4fb41980
Neon860 <neonvaprolit@gmail.com>
2025-03-04 20:51:30 +0500

    Update PlayerUI.cs

--- PlayerUI.cs
+++ PlayerUI.cs
@@ -9,11 +9,14 @@ public class PlayerUI : MonoBehaviour
     public Player player;
     public TextMeshProUGUI coinsCounterText;
     public Slider healthSlider;
-
+    public Mana mana;
+    
     void Update()
     {
         healthSlider.maxValue = player.maxHealth;
         healthSlider.value = player.health;
         coinsCounterText.text = player.coins.ToString();
+        manaSlider.maxValue = mana.maxMana;
+        manaSlider.value = mana.mana;
     }
 }
