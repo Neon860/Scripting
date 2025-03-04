405ee54f2383a24dfeb72d7acf4ce6976e7ef23e
Neon860 <neonvaprolit@gmail.com>
2025-03-04 21:05:00 +0500

    Update Wand.cs

--- Wand.cs
+++ Wand.cs
@@ -5,18 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class Wand : MonoBehaviour
 {
-    public GameObject fireballPrefab;
+        public GameObject fireballPrefab;
     public Transform attackPoint;
     public AudioClip Magic;
     public AudioSource audioMagic;
-
+    public int mana = 10;
+    public int maxMana = 10;
+    public float recoveryTimer = 3;
+    public float recoveryDelay = 3;
+    
     public void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (mana >= 5)
         {
-            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
-            audioMagic.PlayOneShot(Magic);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+                audioMagic.PlayOneShot(Magic);
+                mana -= 5;             
+            }
+        }
 
+        if (mana < 10)
+        {
+            if (recoveryTimer <= 0)
+            {
+                mana += 1;
+                recoveryTimer += recoveryDelay;
+            }
+            else
+            {
+                recoveryTimer -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            mana += 1;
         }
     }
 }
