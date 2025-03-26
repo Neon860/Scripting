313b9b06fce503588c3fc03a91a7f8698cd1bc47
Neon860 <neonvaprolit@gmail.com>
2025-03-26 18:55:05 +0500

    Update CharacterAnim.cs

--- CharacterAnim.cs
+++ CharacterAnim.cs
@@ -13,10 +13,23 @@ public class CharacterAnim : MonoBehaviour
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
     }
-
-    
-    void Update()
+void Update()
     {
-        
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if (forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+        else  animator.SetBool(isWalkingHash, false);
+
+        if (forwardPressed && runPressed)
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+        else animator.SetBool(isRunningHash, false);
     }
 }
