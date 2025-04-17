412fde1e0294ec2fd11323c27d314fd607c1e7df
Neon860 <neonvaprolit@gmail.com>
2025-04-17 19:57:17 +0500

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,46 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int health = 10;
-    public int maxHealth = 10;
-    public GameObject fireballPrefab;
-    public Transform attackPoint;
-    private int coins;
-    public AudioSource audioSourse;
-    public AudioClip collectSound;
-    public AudioClip damageSound;
+    public static int score = 0;
+    public static List<Square> squares;
 
-    public void Update()
+    public static void Defeat()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Instantiate(fireballPrefab,attackPoint.position, attackPoint.rotation);
-        }
+        score = 0;
     }
+
+    private void OnMouseDown()
+    {
         
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-        print("Здаровье игрока: " + health);
-        if (health > 0)
-        {
-            audioSourse.PlayOneShot(damageSound);
-        }
-        else
-        {
-            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(sceneIndex);
-        }
-
     }
-    public void CollectCoins()
+
+    void Awake()
     {
-        coins++;
-        audioSourse.PlayOneShot(collectSound);
-        print("Ñîáðàííûå ìîíåòêè: " + coins);
+       squares = new List<Square>();
+
     }
 }
