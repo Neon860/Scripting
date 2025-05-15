5989cfec9831be9b7835798d902a158db264ec2c
Neon860 <neonvaprolit@gmail.com>
2025-05-15 20:07:04 +0500

    Update Obstacle.cs

--- Obstacle.cs
+++ Obstacle.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    public float scaleMax = 1.3f;
+    public float scaleMin = 0.7f;
+
+    public float rotationMax = 180f;
+    public float rotationMin = 0f;
+
+    public GameObject explosionPlayer;
+    public GameObject player;
+
+    private void Start()
+    {
+        randomSize();
+        randomRotation();
+    }
+    void randomSize()
+    {
+        float scaleFactor = Random.Range(scaleMin, scaleMax);
+        transform.localScale = (Vector2)transform.localScale * scaleFactor;
+    }
+    void randomRotation()
+    {
+        float rotationFactor = Random.Range(rotationMin, rotationMax);
+        transform.localEulerAngles = Vector3.forward * rotationFactor;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            GameObject effect = Instantiate(explosionPlayer, transform.position, transform.rotation);
+            Destroy(effect, 5);
             Destroy(collision.gameObject);
+        }
+
+        if (collision.tag == "Border")
+        {
+            
         }
     }
 }
